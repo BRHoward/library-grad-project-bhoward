7b6b12d89c3e3e31816c422f8776b1b48ae49091
Ben Howard <bhoward@scottlogic.co.uk>
2016-11-01 15:49:58 +0000

    unique id field in reservation model

--- library-grad-project/Models/Reservation.cs
+++ library-grad-project/Models/Reservation.cs
@@ -7,7 +7,7 @@ namespace LibraryGradProject.Models
 {
     public class Reservation
     {
-
+        public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
