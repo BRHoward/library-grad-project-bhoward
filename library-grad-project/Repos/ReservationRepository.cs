bdc84882580f585c688717131edc11b6b0828142
Ben Howard <bhoward@scottlogic.co.uk>
2016-11-01 16:13:04 +0000

    added reservationRepository file

---
+++ library-grad-project/Repos/ReservationRepository.cs
@@ -0,0 +1,32 @@
+﻿using LibraryGradProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryGradProject.Repos
+{
+    public class ReservationRepository : IRepository<Reservation>
+    {
+
+        public void Add(Reservation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
