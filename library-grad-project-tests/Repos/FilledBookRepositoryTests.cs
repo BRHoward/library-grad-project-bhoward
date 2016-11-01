e5d72286c3b438b9f05824743b308fb4ce8a70e0
Ben Howard <bhoward@scottlogic.co.uk>
2016-11-01 15:00:58 +0000

    more filled book repo tests

--- library-grad-project-tests/Repos/FilledBookRepositoryTests.cs
+++ library-grad-project-tests/Repos/FilledBookRepositoryTests.cs
@@ -23,5 +23,95 @@ namespace LibraryGradProjectTests.Repos
             // Assert
             Assert.Equal(new Book[] { book1, book2, book3 }, books.ToArray());
         }
+
+        [Fact]
+        public void Add_Inserts_New_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook = new Book() { Title = "Test" };
+
+            // Act
+            repo.Add(newBook);
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            Assert.Equal(books.Count(), 4);
+        }
+
+        [Fact]
+        public void Add_Sets_New_Id()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook = new Book() { Title = "Test" };
+
+            // Act
+            repo.Add(newBook);
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            Assert.Equal(3, books.Last().Id);
+        }
+
+        [Fact]
+        public void Get_Returns_Specific_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book newBook1 = new Book() { Id = 0, Title = "Test1" };
+            Book newBook2 = new Book() { Id = 1, Title = "Test2" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+
+            // Act
+            Book book = repo.Get(4);
+
+            // Asert
+            Assert.Equal(newBook2, book);
+        }
+
+        [Fact]
+        public void Get_All_Returns_All_Books()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book book1 = new Book { Id = 0, Title = "Tennis", ISBN = "11111", Author = "Andy Murray", PublishDate = "01/01/2001" };
+            Book book2 = new Book { Id = 1, Title = "All about tennis", ISBN = "22222", Author = "Tim Henman", PublishDate = "02/02/2002" };
+            Book book3 = new Book { Id = 2, Title = "Yeah tennis", ISBN = "33333", Author = "Boris Becker", PublishDate = "03/03/2003" };
+            Book newBook1 = new Book() { Title = "Test1" };
+            Book newBook2 = new Book() { Title = "Test2" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+
+            // Act
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            Assert.Equal(new Book[] { book1, book2, book3, newBook1, newBook2 }, books.ToArray());
+        }
+
+        [Fact]
+        public void Delete_Removes_Correct_Book()
+        {
+            // Arrange
+            FilledBookRepository repo = new FilledBookRepository();
+            Book book1 = new Book { Id = 0, Title = "Tennis", ISBN = "11111", Author = "Andy Murray", PublishDate = "01/01/2001" };
+            Book book2 = new Book { Id = 1, Title = "All about tennis", ISBN = "22222", Author = "Tim Henman", PublishDate = "02/02/2002" };
+            Book book3 = new Book { Id = 2, Title = "Yeah tennis", ISBN = "33333", Author = "Boris Becker", PublishDate = "03/03/2003" };
+            Book newBook1 = new Book() { Title = "Test1" };
+            Book newBook2 = new Book() { Title = "Test2" };
+            Book newBook3 = new Book() { Title = "Test3" };
+            repo.Add(newBook1);
+            repo.Add(newBook2);
+            repo.Add(newBook3);
+
+            // Act
+            repo.Remove(4);
+            IEnumerable<Book> books = repo.GetAll();
+
+            // Asert
+            Assert.Equal(new Book[] { book1, book2, book3, newBook1, newBook3 }, books.ToArray());
+        }
     }
 }
