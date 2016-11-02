cefbad2bd7f2164841d0bddc491a87bc4b1bd0a3
Ben Howard <bhoward@scottlogic.co.uk>
2016-11-02 11:10:35 +0000

    basic reservation repo tests

--- library-grad-project/Models/Reservation.cs
+++ library-grad-project/Models/Reservation.cs
@@ -12,5 +12,26 @@ namespace LibraryGradProject.Models
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to book return false.
+            Reservation p = obj as Reservation;
+            if ((System.Object)p == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return
+                (bookId == p.bookId) &&
+                (StartDate == p.StartDate) &&
+                (EndDate == p.EndDate);
+        }
     }
 }
