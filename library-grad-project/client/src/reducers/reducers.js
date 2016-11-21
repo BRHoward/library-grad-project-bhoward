2619532ade898005627aa2a8ad4717b2c686eb7a
Ben Howard <bhoward@scottlogic.co.uk>
2016-11-21 14:43:55 +0000

    removed unused reducer functionality2

--- library-grad-project/client/src/reducers/reducers.js
+++ library-grad-project/client/src/reducers/reducers.js
@@ -1,10 +1,5 @@
 import {combineReducers} from 'redux';
 import {
-	ADD_BOOK, 
-	DELETE_BOOK,
-	UPDATE_BOOK,
-	ADD_RESERVATION, 
-	DELETE_RESERVATION,
     REQUEST_BOOKS,
     RECEIVE_BOOKS,
 	REQUEST_RESERVATIONS,
@@ -15,32 +10,6 @@ function books(state = {
 	items : []
 }, action) {
 	switch (action.type) {
-		case ADD_BOOK :
-			return [
-				...state,
-				{
-					Id : action.payload.id,
-					title : action.payload.book.title,
-					author : action.payload.book.author,
-					publishDate : action.payload.book.publishDate,
-					isbn : action.payload.book.isbn
-				}
-			]
-		case DELETE_BOOK :
-			const bookToDeleteIdx = state.findIndex(book => book.Id === action.payload.id);
-			return [
-				...state.slice(0,bookToDeleteIdx),
-				...state.slice(bookToDeleteIdx+1)
-			]
-		case UPDATE_BOOK : 
-			const bookToUpdateIdx = state.findIndex(book => book.Id === action.payload.id);
-			return [
-				...state.slice(0,bookToUpdateIdx),
-				Object.assign({}, action.payload.book, {
-					Id : state[bookToUpdateIdx].Id
-				}),
-				...state.slice(bookToUpdateIdx+1)
-			] 
 		case REQUEST_BOOKS :
 			return Object.assign({}, state, {
 				isFetching: true
@@ -61,22 +30,6 @@ function reservations(state = {
 		items : []
 }, action) {
 	switch (action.type) {
-		case ADD_RESERVATION :
-			return [
-				...state,
-				{
-					Id: action.payload.id,
-					bookId : action.payload.reservation.bookId,
-					startDate : action.payload.reservation.startDate,
-					endDate : action.payload.reservation.endDate
-				}
-			]
-		case DELETE_RESERVATION :
-			const reservationToDeleteIdx = state.findIndex(reservation => reservation.Id === action.payload.id);
-			return [
-				...state.slice(0,reservationToDeleteIdx),
-				...state.slice(reservationToDeleteIdx+1)
-			]
 		case REQUEST_RESERVATIONS :
 			return Object.assign({}, state, {
 				isFetching: true
